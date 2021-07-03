dd1784edb0fc263ab5cb1e112884ff60b4163e18
PiggyAwesome <48888771+PiggyAwesome@users.noreply.github.com>
2021-07-03 12:02:54 +0200

    added YouTube view bot

---
+++ youtubeviewbot.py
@@ -0,0 +1,28 @@
+import requests
+import threading
+
+url = 'https://www.youtube.com/watch?v=FqQIUPdGg-M'
+
+
+
+def do_request():
+    while True:
+      response = requests.get(url)
+      print(response)
+
+
+
+threads = []
+
+for i in range(500):
+
+ t = threading.Thread(target=do_request())
+ t.daemon = True
+ threads.append(t)
+
+
+for i in range(500):
+ threads[i].start
+
+for i in range(500):
+ threads[i].join
